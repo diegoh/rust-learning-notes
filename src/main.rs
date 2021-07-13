a5a802576cd637fca7db022cd23aad4b2160726c
Diego Herrera <608492+diegoh@users.noreply.github.com>
2021-07-13 21:47:11 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter, Stdout, StdoutLock};
+
+fn main() {
+    let stdout: Stdout = stdout();
+    let message: String = String::from("Hello, world!");
+    let width: usize = message.chars().count();
+    let mut writer: BufWriter<StdoutLock> = BufWriter::new(stdout.lock());
+
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
